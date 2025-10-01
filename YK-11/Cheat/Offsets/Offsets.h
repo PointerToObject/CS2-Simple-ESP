93965ad0f40ee00983fcd90f81012e97514589f2
Alabama Aldi Employee <164882065+PointerToObject@users.noreply.github.com>
2025-10-01 13:05:48 -0500

    Update Offsets.h
    easier to update

--- YK-11/Cheat/Offsets/Offsets.h
+++ YK-11/Cheat/Offsets/Offsets.h
@@ -7,8 +7,8 @@ unsigned __int64 dwLocalPlayerController = 0x1E0D3E8;
 unsigned __int64 dwLocalPlayerPawn = 0x1BDFD10;
 unsigned __int64 dwViewMatrix = 0x1E21090;
 
-
-
-
-
+unsigned __int64 m_bIsLocalPlayerController = 0x778;
+unsigned __int64 m_iszPlayerName = 0x6E8;
+unsigned __int64 m_vecLastClipCameraPos = 0x3DD4;
+unsigned __int64 m_hPlayerPawn = 0x8FC;
 
